7e3cdd0a428cdc8d485557688438af22c9ee3e9a
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-05-22 17:17:23 +0300

    Updated answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,31 @@
 class AnswersController < ApplicationController
+  before_action :find_question
+
+  def show
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def new
+    @answer = @question.answers.build
+  end
+
   def create
-    @question = Question.find(params[:question_id])
-    @question.answers.create(answer_params)
+    @answer = @question.answers.build(answer_params)
+    @answer.user_id = current_user.id
+    @answer.save
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to question_path(@question), notice: 'Successfully deleted your answer!'
   end
 
   private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body)
